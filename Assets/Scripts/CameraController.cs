f550497f723e4229be2d71ee67633534d89098b4
Filip Bergkvist <filip.0717@hotmail.com>
2018-01-16 16:02:11 +0100

    Add Player_Test scene with controls and camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField]
+	private Transform playerTop;
+    [SerializeField]
+    private Transform playerBot;
+    [SerializeField]
+    private float followSpeed = 30f;
+    [SerializeField]
+    private float offset;
+    [SerializeField]
+    private float deadZone;
+
+    [SerializeField]
+    private float midPlayers;
+    [SerializeField]
+    private float camDistance;
+
+    private void FixedUpdate()
+    {
+        /*
+        // Get the X distance from the camera to the players
+        float midPlayers = playerTop.position.x - playerBot.position.x;
+
+        float distanceTop = playerTop.position.x - transform.position.x + offset;
+        float distanceBot = playerBot.position.x - transform.position.x + offset;
+
+        if (distanceTop > deadZone && distanceBot > deadZone)
+            transform.Translate(new Vector2((distanceTop - deadZone) / followSpeed, 0.0f), Space.Self);      // Move Right
+        else if (distanceTop < -deadZone && distanceBot < -deadZone)
+            transform.Translate(new Vector2((distanceTop + deadZone) / followSpeed, 0.0f), Space.Self);      // Move Left
+
+        */
+
+        // Get the X distance from the camera to the players
+        midPlayers = playerTop.position.x + (playerBot.position.x - playerTop.position.x) / 2;
+        camDistance = midPlayers - transform.position.x;
+        //transform.position = new Vector3(midPlayers, 0f, -10f);
+
+        if (camDistance > deadZone && camDistance > deadZone)
+            transform.Translate(new Vector2((camDistance - deadZone) / followSpeed, 0.0f), Space.Self);      // Move Right
+        else if (camDistance < -deadZone && camDistance < -deadZone)
+            transform.Translate(new Vector2((camDistance + deadZone) / followSpeed, 0.0f), Space.Self);      // Move Left
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [Header("Movement")]
+    [SerializeField]
+    private string movementAxis = "HorizontalTop";
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private string jumpAxis = "JumpTop";    // Implement with ground checking
+    [SerializeField]
+    private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField]
+    private float jumpHeight = 10f;
+
+    private float horizontal;
+
+    [Header("Physics")]
+    [SerializeField]
+    private bool upsideDown = false;
+    [SerializeField]
+    private float gravityScale = 1f;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Reverse gravity if upside down
+
+
+        if(upsideDown)
+        {
+            gravityScale = -gravityScale;
+            jumpHeight = -jumpHeight;
+        }
+
+        rb.gravityScale = gravityScale;
+    }
+	
+	void Update ()
+    {
+        horizontal = Input.GetAxisRaw(movementAxis);
+        if(Input.GetKeyDown(jumpKey))
+        {
+            rb.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(horizontal * speed, 0f), ForceMode2D.Impulse);
+    }
+}
