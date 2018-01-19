50e109a57a9bf25aae9111502334e353754379bd
Serover <35572082+Serover@users.noreply.github.com>
2018-01-19 20:15:42 +0100

    Add files via upload

---
+++ Assets/Scripts/GiveTagAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GiveTagAttribute : PropertyAttribute
+{
+    // Data being sent to Drawer...
+
+
+    // REASONING 
+
+    // Since the Drawer uses existing data infrom of Taglist
+    // no data is actually needed to be sent, but to use a Drawer (atleast to extent I want too, the attribute way)
+    // you have to asign a porpertyAttribute, therefore we have this empty attribute.
+}
---
+++ Assets/Scripts/TagDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(GiveTagAttribute))]
+public class TagDrawer : PropertyDrawer
+{
+    // Overide GUI, a must incase you wanna change the GUI ...
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Is it a Seralized String? ( in simple, is it a public string? )
+        if (property.propertyType == SerializedPropertyType.String)
+        {
+            //Begin Change
+            EditorGUI.BeginProperty(position, label, property);
+
+            // using the existing TagField, which creates a window of the existing tags. 
+            // and then simply change the value to the selected String (Tag).
+            property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+
+            // End Change
+            EditorGUI.EndProperty();
+        }
+        else // Incase it's not a seralized String use standard "drawing".
+            EditorGUI.PropertyField(position, property, label);
+    }
+}
