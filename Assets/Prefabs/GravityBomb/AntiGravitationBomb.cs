b3ea4625235ccfaf686fa06beaeb989045721e59
Xiao Liang <35493674+Diamondspader@users.noreply.github.com>
2018-01-29 17:45:37 +0100

    add gravitybomb
    gravitybomb scripts & testprefabs

---
+++ Assets/Prefabs/GravityBomb/AntiGravitationBomb.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiGravitationBomb : MonoBehaviour 
+{
+	[Header("Attributes")]
+	public int range;
+	public int explosionForce;
+	public float duration;
+
+	[Header("tags from objects to pull")]
+	[GiveTag]
+	public string[] tagNames;
+
+	private List<GameObject> pullObjects;
+	private Rigidbody2D rb;
+	private bool buttonPressed;
+	private bool targetting;
+	private bool antiGravitationActivated;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		pullObjects = new List<GameObject> ();
+		buttonPressed = true;
+		antiGravitationActivated = false;
+		targetting = false;
+
+	}
+
+	void Update () 
+	{
+		ActivateAntiGravitationBomb ();
+
+	}
+
+	void ActivateAntiGravitationBomb()
+	{
+		if (Input.GetKeyDown (KeyCode.O) && buttonPressed) 
+		{
+			buttonPressed = false;
+			targetting = true;
+			TargetInRange ();
+			antiGravitationActivated = true;
+			AntiGravitationPull ();
+			rb.bodyType = RigidbodyType2D.Static;
+			Destroy (gameObject);
+		}
+		Destroy (gameObject, duration);
+	}
+
+	void TargetInRange()
+	{
+		if (targetting) 
+		{
+			foreach (string tag in tagNames) 
+			{
+				GameObject[] o = GameObject.FindGameObjectsWithTag (tag);
+				foreach (GameObject targetObjects in o) 
+				{
+					float inRange = Vector3.Distance (transform.position, targetObjects.transform.position);
+					if (range > inRange) 
+					{
+						pullObjects.Add (targetObjects);
+					}
+				}
+			}
+			targetting = false;
+		}
+	}
+
+	void AntiGravitationPull()
+	{
+		if (antiGravitationActivated) 
+		{
+			foreach (GameObject toPull in pullObjects) 
+			{
+				toPull.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+				ShockWave (toPull, explosionForce);
+			}
+		}
+	}
+
+	void ShockWave(GameObject obj, float push)
+	{
+		float distance = Vector3.Distance(transform.position, obj.transform.position);
+		float xDirection = (obj.transform.position.x - transform.position.x);
+		float yDirection = (obj.transform.position.y - transform.position.y);
+		Vector2 direction = new Vector2(xDirection, yDirection);
+
+		Vector2 theForce = direction.normalized * obj.GetComponent<Rigidbody2D> ().mass * push -
+			direction.normalized* push * obj.GetComponent<Rigidbody2D> ().mass / -distance;
+
+		obj.GetComponent<Rigidbody2D> ().AddForce (theForce);
+		Debug.Log (obj.name + theForce + distance + direction);
+
+	
+	}
+}
---
+++ Assets/Prefabs/GravityBomb/InstantiateGravityBomb.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateGravityBomb : MonoBehaviour 
+{
+
+	public GameObject gravityBomb;
+	public float speed;
+	public float cooldown;
+
+	private Vector2 direction;
+	private bool reload;
+	private float setCooldown;
+
+
+	void Start()
+	{
+		direction = new Vector2 (1,1);
+		reload = true;
+		setCooldown = cooldown;
+	}
+
+	void Update()
+	{
+		
+			Fire (direction, gravityBomb);
+		
+	}
+
+
+	void Fire(Vector2 Direction, GameObject fireObj)
+	{
+		if (Input.GetKeyDown (KeyCode.P) && reload) {	
+			GameObject clone = (GameObject)Instantiate (fireObj, transform.position, Quaternion.identity) as GameObject;
+			clone.GetComponent<Rigidbody2D> ().AddForce (Direction * speed);
+			reload = false;
+			setCooldown = cooldown;
+		}
+		Reload ();
+	}
+
+
+
+	void Reload()
+	{
+		setCooldown -= Time.deltaTime;
+		if (setCooldown < 0 && !reload) {
+			reload = true;
+		}
+	}
+
+}
