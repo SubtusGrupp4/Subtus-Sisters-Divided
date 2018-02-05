a1e043a17fc48d8426e66a4068ff85d091fa09a2
Xiao Liang <35493674+Diamondspader@users.noreply.github.com>
2018-02-05 16:02:06 +0100

    added Portalbehaviours in gravity_Test

---
+++ Assets/Prefabs/GravityBomb/EnemyPortal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPortal : PortalBehaviour {
+
+	public GameObject reversedObject;
+	private GameObject clone;
+	private float timer = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+			GetComponent<Collider2D> ().enabled = true;
+		}
+	}
+
+	public override void OnPortalContact(){
+
+
+		clone = Instantiate (reversedObject, transform.position, Quaternion.identity) as GameObject;
+
+		Rigidbody2D cloneGravity = clone.GetComponent<Rigidbody2D> ();
+		cloneGravity.velocity = this.GetComponent<Rigidbody2D> ().velocity;
+		clone.GetComponent<Collider2D> ().enabled = false;
+		cloneGravity.gravityScale *= -this.GetComponent<Rigidbody2D>().gravityScale;
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Prefabs/GravityBomb/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called rant
+
+
+	void OnTriggerExit2D(Collider2D otherObj)
+	{
+		if (otherObj.gameObject.GetComponent<PortalBehaviour>() != null) {
+			otherObj.GetComponent<PortalBehaviour> ().OnPortalContact ();
+		}
+	}
+
+
+}
