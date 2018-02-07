c04607bd674e16e5cf4864c98d8a514a417f5a71
Serover <erik.j1998@hotmail.com>
2018-02-07 14:38:53 +0100

    ButtonDoor

---
+++ Assets/Scripts/ButtonOpenDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonOpenDoor : BaseButton {
+
+    public List<GameObject> objects = new List<GameObject>();
+
+
+    protected override void DoStuff()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].GetComponent<Door>().Toggle();
+        }
+    }
+
+    protected override void UndoStuff()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].GetComponent<Door>().Toggle();
+        }
+        
+    }
+}
