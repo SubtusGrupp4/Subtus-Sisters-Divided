7a14bd267d50792b1fc4feaae66784dd897b870a
Zufixx <filip.0717@hotmail.com>
2018-02-08 16:41:48 +0100

    Working safepoints, unfinished but working

--- Assets/Scripts/Revive/Checkpoint.cs
+++ Assets/Scripts/Revive/Checkpoint.cs
@@ -1,37 +1,13 @@
 ﻿using System.Collections; 
 using System.Collections.Generic; 
-using UnityEngine;
+using UnityEngine; 
 
 public class Checkpoint : MonoBehaviour 
 {
-    [SerializeField]
-    private Vector2 childOffset = Vector2.zero;
-    public Transform child;
-    private Color color = Color.yellow;
-
-    private void OnTriggerEnter2D(Collider2D other)
-	{
-		if(other.tag == "Player") 
-		{
-			if(child == null)
-				child = transform.GetChild(0);
-
-            PushCheckpointManager();
-        }
-	}
-
-    public virtual void PushCheckpointManager() 
+    /*
+    public override void PushCheckpointManager()
     {
-        CheckpointManager.instance.Checkpoint(transform, child.transform);
+        SafepointManager.instance.Checkpoint(transform, child.transform);
     }
-
-	private void OnDrawGizmos() 
-	{
-        child = transform.GetChild(0);
-        child.transform.position = new Vector2(transform.position.x, -transform.position.y) + childOffset;
-
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(new Vector3(transform.position.x, -11f),
-                new Vector3(transform.position.x, 11f));
-	}
+    */
 }
--- Assets/Scripts/Revive/Safepoint.cs
+++ Assets/Scripts/Revive/Safepoint.cs
@@ -1,11 +1,64 @@
 ﻿using System.Collections; 
 using System.Collections.Generic; 
-using UnityEngine; 
+using UnityEngine;
 
-public class Safepoint : Checkpoint 
+public class Safepoint : MonoBehaviour 
 {
-    public override void PushCheckpointManager()
+    [Header("Activation")]
+    [SerializeField]
+    private Sprite collectedSprite;
+    protected Sprite startSprite;
+
+    public bool isCurrent = false;
+
+    protected SpriteRenderer sr;
+
+    [Header("Exit Timer")]
+    public bool playerExited = false;
+    [SerializeField]
+    private float exitTime = 5f;
+    [SerializeField]
+    [Range(0f, 1f)]
+    protected float timer = 1f;
+
+    private void Start()
     {
-        CheckpointManager.instance.Safepoint(transform, child.transform);
+        sr = GetComponent<SpriteRenderer>();
+        startSprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    private void Update()
+    {
+        // If the player has exited the trigger and the safepoint is not the current spawnpoint
+        if (playerExited && !isCurrent)
+        {
+            // Start a timer towards deactivating the safepoint
+            if (timer > 0f)
+                timer -= Time.deltaTime / exitTime;
+            else
+            {
+                // When it is done, deactivate the safepoint and change the sprite
+                SafepointManager.instance.SetAsDeactivated(transform);
+                sr.sprite = startSprite;
+                timer = 0f;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player" && !isCurrent)                     // If a player enters the trigger
+		{
+            playerExited = false;                                   // Stop the timer
+            timer = 1f;                                             // Reset the timer
+            sr.sprite = collectedSprite;                            // Change the sprite
+            SafepointManager.instance.SetAsActivated(transform);    // Tell the manager that his safepoint is activated
+        }
+	}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player" && !isCurrent)                    // If a player exits the trigger
+            playerExited = true;                                    // Start the timer
     }
 }
