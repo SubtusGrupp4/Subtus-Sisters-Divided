10fc4bdd8c0fbeacbd09170b5b2730cd933aed9b
Zufixx <filip.0717@hotmail.com>
2018-02-09 11:54:23 +0100

    Add Autosave, not tested

---
+++ Assets/Scripts/AutosaveScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using System;
+
+public class AutosaveScene : MonoBehaviour 
+{
+    public bool doAutoSave = false;
+    public bool showMessage = true;
+    public bool isStarted = false;
+    public int intervalScene = 2;
+    public DateTime lastSaveTimeScene = DateTime.Now;
+
+    public string projectPath;
+}
