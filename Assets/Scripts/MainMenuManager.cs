8c4d9f7558c4c767020c9389abfb3ff2507c646b
Zufixx <filip.0717@hotmail.com>
2018-02-17 14:21:23 +0100

    Add initial main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class MainMenuManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject lightUI;
+	[SerializeField]
+	private GameObject darkUI;
+
+	[SerializeField]
+	private GameObject light;
+	[SerializeField]
+	private GameObject dark;
+
+	[SerializeField]
+	private GameObject pressAnyKey;
+
+	private bool useDark = false;
+	private bool isActivated = false;
+	private bool isDonePlaying = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(KeyTimer());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.anyKey && !isActivated) 
+		{
+			lightUI.SetActive(true);
+			lightUI.GetComponentInChildren<PlayableDirector>().Play();
+			StartCoroutine(DonePlaying((float)lightUI.transform.GetChild(0).GetComponent<PlayableDirector>().duration));
+			pressAnyKey.SetActive(false);
+			isActivated = true;
+		}
+
+		for(int i = 0; i < lightUI.transform.childCount; i++) 
+		{
+			darkUI.transform.GetChild(i).position = lightUI.transform.GetChild(i).position;
+		}
+
+		if(isDonePlaying) 
+		{
+			if(isActivated && useDark) 
+			{
+				dark.SetActive(true);
+				light.SetActive(false);
+			}
+			else if(isActivated) 
+			{
+				dark.SetActive(false);
+				light.SetActive(true);
+			}
+
+			if(Input.GetKeyDown(KeyCode.P) && isActivated)
+				useDark = !useDark;
+			}
+	}
+
+	IEnumerator KeyTimer() 
+	{
+		yield return new WaitForSeconds(5f);
+		if(!isActivated)
+			pressAnyKey.SetActive(true);
+			
+	}
+
+	IEnumerator DonePlaying(float animationLength) 
+	{
+		yield return new WaitForSeconds(animationLength);
+		isDonePlaying = true;
+	}
+}
---
+++ Assets/Scripts/RandomMoveInSphere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMoveInSphere : MonoBehaviour {
+
+	public float frequency = 0.1f;
+    public float strength = 1f;
+
+    private Vector3 startPos;
+	[SerializeField]
+    private Vector2 randomDir;
+	private float timeSinceRandomRefresh = 1f;
+
+	private Rigidbody2D rb;
+
+	void Start () 
+	{
+		startPos = transform.position;
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate()
+	{
+		rb.AddForce(startPos - transform.position * strength);
+
+		if(timeSinceRandomRefresh >= 1.5f) 
+		{
+			randomDir = Random.insideUnitSphere;
+			timeSinceRandomRefresh = 0f;
+		} 
+		else 
+		{
+			timeSinceRandomRefresh += Time.deltaTime / frequency;
+			rb.AddForce(new Vector2(randomDir.x, randomDir.y) * strength);
+		}
+	}
+}
