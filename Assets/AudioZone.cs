a9b3d01102a52b4c81444ecc5f2230de999a4e77
Zufixx <filip.0717@hotmail.com>
2018-02-27 17:09:49 +0100

    Add FMOD footsteps and audio zones

---
+++ Assets/AudioZone.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Collider))]
+public class AudioZone : MonoBehaviour
+{
+    [System.Serializable]
+    private struct Parameter
+    {
+        public string name;
+        [Range(0f, 1f)]
+        public float value;
+    }
+
+    [SerializeField]
+    FMODUnity.StudioEventEmitter emitter;
+
+    [SerializeField]
+    private List<Parameter> parameters;
+
+    private List<Parameter> oldParameters;
+
+    [SerializeField]
+    private bool resetOnExit = false;
+    [SerializeField]
+    private bool fetchParameters = true;
+
+    private void Start()
+    {
+        if (resetOnExit) {
+            for (int i = 0; i < emitter.Params.Length; i++)
+            {
+                Parameter p = new Parameter
+                {
+                    name = emitter.Params[i].Name,
+                    value = emitter.Params[i].Value
+                };
+
+                oldParameters.Add(p);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        if (fetchParameters)
+        {
+            parameters.Clear();
+            for (int i = 0; i < emitter.Params.Length; i++)
+            {
+                Parameter p = new Parameter
+                {
+                    name = emitter.Params[i].Name,
+                    value = emitter.Params[i].Value
+                };
+
+                parameters.Add(p);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            foreach (Parameter p in parameters)
+                emitter.SetParameter(p.name, p.value);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && resetOnExit)
+        {
+            foreach (Parameter p in oldParameters)
+                emitter.SetParameter(p.name, p.value);
+        }
+    }
+}
--- Assets/Scripts/MovementAudio.cs
+++ Assets/Scripts/MovementAudio.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GroundType
+{
+    Grass, Gravel, Wood,
+    GrassGross
+}
+
 public class MovementAudio : MonoBehaviour
 {
+    public GroundType groundType = GroundType.Grass;
+
     [FMODUnity.EventRef]
-    public string eventName;
+    public string grassEvent;
+    [FMODUnity.EventRef]
+    public string gravelEvent;
+    [FMODUnity.EventRef]
+    public string woodEvent;
+    [FMODUnity.EventRef]
+    public string grassGrossEvent;
+
     public FMOD.Studio.EventInstance eventInstance;
     public FMOD.Studio.ParameterInstance parameterInstance;
+    [SerializeField]
+    FMODUnity.StudioEventEmitter emitter;
 
     //[SerializeField]
     //private AudioClip[] footsteps;
@@ -28,12 +45,33 @@ public class MovementAudio : MonoBehaviour
 
         //Debug.Log("Play Footsteps");
         //FMODUnity.RuntimeManager.PlayOneShot(eventPath);
-        //FMODUnity.RuntimeManager.PlayOneShot(eventPath);
-        //Helloooooo
 
-        eventInstance = FMODUnity.RuntimeManager.CreateInstance(eventName);
-        eventInstance.getParameter("Grass", out parameterInstance);
-        eventInstance.start();
-        parameterInstance.setValue(1f);
+        switch(groundType)
+        {
+            case GroundType.Grass:
+                Debug.Log("Grass");
+                FMODUnity.RuntimeManager.PlayOneShot(grassEvent, transform.position);
+                break;
+            case GroundType.Gravel:
+                Debug.Log("Gravel");
+                FMODUnity.RuntimeManager.PlayOneShot(gravelEvent, transform.position);
+                break;
+            case GroundType.Wood:
+                Debug.Log("Wood");
+                FMODUnity.RuntimeManager.PlayOneShot(woodEvent, transform.position);
+                break;
+            case GroundType.GrassGross:
+                Debug.Log("Grass Gross");
+                FMODUnity.RuntimeManager.PlayOneShot(grassGrossEvent, transform.position);
+                break;
+        }
+
+        //eventInstance = FMODUnity.RuntimeManager.CreateInstance(eventName);
+        //eventInstance.getParameter("Grass", out parameterInstance);
+        //eventInstance.start();
+        //parameterInstance.setValue(1f);
+
+        //emitter.Play();
+        //emitter.SetParameter("Grass", 1f);
     }
 }
