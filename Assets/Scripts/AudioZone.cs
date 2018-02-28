651c55c621c5ec5adfb55293b993bc5b2807c49a
Zufixx <filip.0717@hotmail.com>
2018-02-28 10:39:34 +0100

    Improved AudioZone script

--- Assets/Scripts/AudioZone.cs
+++ Assets/Scripts/AudioZone.cs
@@ -18,15 +18,21 @@ public class AudioZone : MonoBehaviour
     [Tooltip("What emitter to target and change.")]
     FMODUnity.StudioEventEmitter emitter;
 
+    [Header("Parameters")]
     [SerializeField]
     [Tooltip("Each parameter will change the corresponding parameter in FMOD")]
     private List<Parameter> parameters;
 
     private List<Parameter> oldParameters;
 
+    [Header("Settings")]
     [SerializeField]
     [Tooltip("If true, will return all values to the initial values from when the scene started when the player exits the trigger.")]
     private bool resetOnExit = false;
+
+    [SerializeField]
+    [Tooltip("If true, will set the starting values (that it resets to when exiting) on entry, instead on when the game starts.")]
+    private bool setStartOnEnter = false;
 
     [SerializeField]
     [Tooltip("This will clear the parameters and fetch all parameters from FMOD. If it doesn't update, play and then stop the game. It should fetch the parameters. If it still doesn't, make sure that under 'Initial Paremeters' in the event emitter component, that all parameters are checked.")]
@@ -34,8 +40,9 @@ public class AudioZone : MonoBehaviour
 
     private void Start()
     {
-        if (resetOnExit)
+        if (resetOnExit && !setStartOnEnter)
         {
+            oldParameters.Clear();
             for (int i = 0; i < emitter.Params.Length; i++)
             {
                 Parameter p = new Parameter
@@ -73,6 +80,21 @@ public class AudioZone : MonoBehaviour
         if (collision.tag == "Player")
             foreach (Parameter p in parameters)
                 emitter.SetParameter(p.name, p.value);
+
+        if (resetOnExit && setStartOnEnter)
+        {
+            oldParameters.Clear();
+            for (int i = 0; i < emitter.Params.Length; i++)
+            {
+                Parameter p = new Parameter
+                {
+                    name = emitter.Params[i].Name,
+                    value = emitter.Params[i].Value
+                };
+
+                oldParameters.Add(p);
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
