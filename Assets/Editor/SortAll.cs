e13b43c84dfb1ab6296973d7542173d6350baef2
Zufixx <filip.0717@hotmail.com>
2018-02-28 11:55:33 +0100

    Sorting order based on position

---
+++ Assets/Editor/SortAll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SortAll : Editor
+{
+    [MenuItem("4K Studios Tools/Sort All")]
+    private static void SetSorting ()
+    {
+        GameObject[] gameObjects = FindObjectsOfType<GameObject>();
+        foreach(GameObject go in gameObjects)
+        {
+            if (go.GetComponent<SpriteRenderer>() != null)
+            {
+                go.GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(go.transform.position.x * go.transform.position.y);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/DynamicSort.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DynamicSort : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update () {
+        GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.x);
+	}
+}
