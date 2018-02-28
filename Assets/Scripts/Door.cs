917056e5a81362c8018d8ac59b33e1585beecbad
Serover <erik.j1998@hotmail.com>
2018-02-28 17:17:35 +0100

    Door updated to Design document, small Anim Update aswell as some prefab updates

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public bool startOpen;
+
+    [GiveTag]
+    public string[] OpenOnTouchWith = new string[] { };
+    public bool autoClose;
+    [SerializeField]
+    private float timeUntilClose;
+
+    [SerializeField]
+    private bool useItemIndex;
+    public int itemIndex;
+
     private bool open = false;
+    private bool currentlyAtDoor;
+    
+    private float timer;
 
     [Header("Images")]
     public Sprite openImage;
@@ -25,10 +39,28 @@ public class Door : MonoBehaviour
         collider = GetComponent<BoxCollider2D>();
         sRender = GetComponent<SpriteRenderer>();
 
+
         if (startOpen)
             Open();
         else
             Close();
+    }
+
+    private void Update()
+    {
+        if (autoClose && currentlyAtDoor == false)
+        {
+            if (open)
+                Close();
+        }
+
+        if (currentlyAtDoor)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+                currentlyAtDoor = false;
+        }
     }
 
     public void Toggle()
@@ -41,17 +73,51 @@ public class Door : MonoBehaviour
 
     public void Open()
     {
-        sRender.sprite = openImage;
-        collider.enabled = false;
-        myAudio.PlayOneShot(openDoorSound);
-        open = true;
+        if (!open)
+        {
+            sRender.sprite = openImage;
+            collider.enabled = false;
+            myAudio.PlayOneShot(openDoorSound);
+            open = true;
+
+            currentlyAtDoor = true;
+            timer = timeUntilClose;
+        }
     }
 
     public void Close()
     {
-        sRender.sprite = closedImage;
-        collider.enabled = true;
-        myAudio.PlayOneShot(closeDoorSound);
-        open = false;
+        if (open)
+        {
+            sRender.sprite = closedImage;
+            collider.enabled = true;
+            myAudio.PlayOneShot(closeDoorSound);
+            open = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D obj)
+    {
+        for (int i = 0; i < OpenOnTouchWith.Length; i++)
+        {
+            if (obj.transform.tag == OpenOnTouchWith[i])
+            {
+                if (obj.transform.GetComponent<ItemIndex>())
+                {
+                    if (useItemIndex)
+                    {
+                        if (obj.transform.GetComponent<ItemIndex>().Index != itemIndex)
+                            break;
+                    }
+
+                    if ((obj.transform.GetComponent<ItemIndex>().DestroyOnUse))
+                        Destroy(obj.transform.gameObject);
+                }
+
+                currentlyAtDoor = true;
+                timer = timeUntilClose;
+                Open();
+            }
+        }
     }
 }
