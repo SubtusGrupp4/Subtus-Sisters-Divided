23aa1717447bd4e07bf54a40a94b175e20283cb8
Zufixx <filip.0717@hotmail.com>
2018-03-01 15:25:40 +0100

    Fixed reviving. Should only spawn when high enough for the player to reach.

--- Assets/Scripts/Revive/RevivePlacer.cs
+++ Assets/Scripts/Revive/RevivePlacer.cs
@@ -7,8 +7,9 @@ public class RevivePlacer : MonoBehaviour
 {
     private Vector2 rayDirection;
 
-    private float moveAmount = 0.05f;
+    private float moveAmount = -0.25f;
     private float maxDistance = 15f;
+    private float rayDistance = 2.25f;
 
     [SerializeField]
     private GameObject reviveSpot;
@@ -19,21 +20,158 @@ public class RevivePlacer : MonoBehaviour
     [SerializeField]
     private bool useDebugDelay = true;
 
+    private bool search = true;
+
     // Called by the player that dies
     public void Initialize (Controller player) 
     {
         // Change the direction based on what player died
+        // If player one died, search up
         if (player == Controller.Player1)
-            rayDirection = Vector2.up;
-        else 
         {
-            moveAmount = -moveAmount;
+            rayDirection = Vector2.up;
+            rayDistance = 3.25f;
+        }
+        else
+        {
             rayDirection = Vector2.down;
+            moveAmount = -moveAmount;
         }
 
         Raycast ();
     }
 
+    private void Raycast () 
+    {
+        if (useDebugDelay)
+            StartCoroutine(SlowRaycast());
+        else
+        {
+            while (search)
+            {
+                transform.position += new Vector3(0f, moveAmount);
+
+                if (Mathf.Abs(transform.position.y) > maxDistance)
+                {
+                    transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+                    passedThrough = false;
+                }
+
+                Vector2 rayOrigin = transform.position;
+
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+
+                if (hit.transform != null)
+                {
+                    // Criterias for what is hit
+                    if (hit.transform.tag == "Player")
+                        continue;
+                    if (hit.transform.tag == "Portal")
+                    {
+                        transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+                        passedThrough = false;
+                        continue;
+                    }
+                    if (hit.collider.isTrigger)
+                        continue;
+
+                    if (hit.transform.position.y < 0f && moveAmount > 0f)
+                        continue;
+                    else if (hit.transform.position.y > 0f && moveAmount < 0f)
+                        continue;
+
+                    passedThrough = true;
+                }
+
+                if (hit.transform == null)
+                {
+                    if (passedThrough == true)
+                    {
+                        Instantiate(reviveSpot, transform.position, Quaternion.identity);
+                        search = false;
+                        Destroy(gameObject);
+                    }
+                }
+            }
+        }
+    }
+
+    // Rewrite
+    private IEnumerator SlowRaycast()
+    {
+        transform.position += new Vector3(0f, moveAmount);
+
+        if (Mathf.Abs(transform.position.y) > maxDistance)
+        {
+            transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+            passedThrough = false;
+        }
+
+        Vector2 rayOrigin = transform.position;
+
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+        Debug.Log("Raycasting...");
+
+        if(hit.transform != null)
+        {
+            Debug.Log("Hit: " + hit.transform.name + ", pos: " + hit.transform.position);
+            // Criterias for what is hit
+            if (hit.transform.tag == "Player")
+            {
+                // Replace with "Continue" in the while loop
+                Debug.Log("Hit player");
+                StartCoroutine(SlowRaycast());
+                yield break;
+            }
+            if(hit.transform.tag == "Portal")
+            {
+                Debug.Log("Hit portal");
+                transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+                passedThrough = false;
+                StartCoroutine(SlowRaycast());
+                yield break;
+            }
+            if(hit.collider.isTrigger)
+            {
+                Debug.Log("Hit trigger");
+                StartCoroutine(SlowRaycast());
+                yield break;
+            }
+            if(hit.transform.position.y < 0f && moveAmount > 0f)
+            {
+                Debug.Log("Hit block on the wrong side (down)");
+                StartCoroutine(SlowRaycast());
+                yield break;
+            }
+            if(hit.transform.position.y > 0f && moveAmount < 0f)
+            {
+                Debug.Log("Hit block on the wrong side (up)");
+                StartCoroutine(SlowRaycast());
+                yield break;
+            }
+
+            Debug.Log("Passed through = true");
+            passedThrough = true;
+        } 
+
+        if(hit.transform == null)
+        {
+            Debug.Log("Hit nothing");
+            if(passedThrough == true)
+            {
+                Debug.Log("Had passed through. Spawning thing");
+                Instantiate(reviveSpot, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+
+        yield return new WaitForSeconds(0.05f);
+        StartCoroutine(SlowRaycast());
+    }
+}
+
+/*
     private void Raycast () 
     {
         if (useDebugDelay)
@@ -49,92 +187,84 @@ public class RevivePlacer : MonoBehaviour
 
             while (search)
             {
-                // Cast a ray looking for blocks
-                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
-
-                if (hit.transform != null)  // If it hits something
+                if(Mathf.Abs(transform.position.y) > maxDistance)
                 {
-                    // Check if player or block on opposite side
-                    if (hit.transform.tag != "Player" && moveAmount < 0f && hit.transform.position.y > 0f || hit.transform.tag != "Player" && moveAmount > 0f && hit.transform.position.y < 0f)
-                    {
-
-                        // If over the max distance, or hit portal
-                        if (Mathf.Abs(rayOrigin.y) > maxDistance || hit.transform.tag == "Portal")
-                        {
-                            transform.position = new Vector2(transform.position.x - 0.5f, 0f);       // Move slightly to the left. TODO: Better solution?
-                            passedThrough = false;
-                        }
-                        if (hit.transform != null && !hit.collider.isTrigger && hit.transform.tag != "Portal") // If it hits something
-                            passedThrough = true;// It has passed through a block
-                    }
-                }
-                else if (passedThrough)  // After passing through one block and then not finding anything
-                    search = false;     // This spot is safe
-
-                transform.position -= new Vector3(0f, moveAmount);     // Move slightly
-                rayOrigin = transform.position;                         // Reset the ray origin to the current position
-            }
-            if (!search) // If a safe spot is found
-            {
-                Instantiate(reviveSpot, transform.position + new Vector3(0f, 0.5f), Quaternion.identity);  // Instantiate the respawn pickup
-                Destroy(gameObject);                                               // Destroy the current object (the placer)
-            }
-        }
-    }
-
-    private IEnumerator SlowRaycast()
-    {
-        Vector2 rayOrigin = transform.position;
-        bool search = true;
-        float rayDistance = 0.5f;
-
-        // TODO: Set this correctly. Should for example not detect the players
-        //LayerMask layer = 1 << 0;
-
-        // Cast a ray looking for blocks
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
-        Color color = hit ? Color.green : Color.red;
-        Debug.DrawRay(rayOrigin, rayDirection, color);
-        Debug.Log("Casting ray");
-
-        if (hit.transform != null)  // If it hits something
-        {
-            Debug.Log("Hit something");
-            if (hit.transform.tag != "Player" && moveAmount < 0f && hit.transform.position.y > 0f || hit.transform.tag != "Player" && moveAmount > 0f && hit.transform.position.y < 0f)  // And it isn't the player
-            {
-                Debug.Log("Not the player or a block on the wrong side!");
-                // If over the max distance, or hit portal
-                if (Mathf.Abs(rayOrigin.y) > maxDistance || hit.transform.tag == "Portal")
-                {
-                    Debug.Log("Dangerous; Move to the left");
-                    transform.position = new Vector2(transform.position.x - 0.5f, 0f);       // Move slightly to the left. TODO: Better solution?
+                    transform.position = new Vector2(transform.position.x - 0.5f, 0f);
                     passedThrough = false;
                 }
-                if (hit.transform != null && !hit.collider.isTrigger && hit.transform.tag != "Portal") // If it hits something
+
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+
+                if (hit.transform != null)
                 {
-                    Debug.Log("passedThrough = true");
-                    passedThrough = true;// It has passed through a block
+                    if (hit.transform.tag != "Player" && moveAmount > 0f && hit.transform.position.y > 0f || hit.transform.tag != "Player" && moveAmount < 0f && hit.transform.position.y < 0f)
+                    {
+
+                        if (hit.transform.tag == "Portal")
+                        {
+                            transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+                            passedThrough = false;
+                        }
+                        if (hit.transform != null && !hit.collider.isTrigger && hit.transform.tag != "Portal")
+                            passedThrough = true;
+                    }
                 }
+                else if (passedThrough)
+                    search = false;
+
+                transform.position -= new Vector3(0f, moveAmount);
+                rayOrigin = transform.position;
+            }
+            if (!search)
+            {
+                Instantiate(reviveSpot, transform.position, Quaternion.identity);
+                Destroy(gameObject);
             }
         }
-        else if (passedThrough)  // After passing through one block and then not finding anything
-        {
-            Debug.Log("Found a safe spot at " + transform.position + ", search = false");
-            search = false;     // This spot is safe
-        }
-
-        Debug.Log("Moving slightly up");
-        transform.position -= new Vector3(0f, moveAmount);     // Move slightly
-
-        yield return new WaitForSeconds(0.01f);
-
-        if (!search) // If a safe spot is found
-        {
-            Debug.Log("Spawning revive spot");
-            Instantiate(reviveSpot, transform.position + new Vector3(0f, 0.5f), Quaternion.identity);  // Instantiate the respawn pickup
-            Destroy(gameObject);                                               // Destroy the current object (the placer)
-        }
-        else
-            Raycast();
     }
-}
+
+    Vector2 rayOrigin = transform.position;
+    bool search = true;
+    float rayDistance = 0.5f;
+
+    // TODO: Set this correctly. Should for example not detect the players
+    //LayerMask layer = 1 << 0;
+
+    if (Mathf.Abs(transform.position.y) > maxDistance)
+    {
+        transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+        passedThrough = false;
+    }
+
+    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+    Color color = hit ? Color.green : Color.red;
+    Debug.DrawRay(rayOrigin, rayDirection, color);
+
+    if (hit.transform != null)
+    {
+        if (hit.transform.tag == "Portal")
+        {
+            transform.position = new Vector2(transform.position.x - 0.5f, 0f);
+            passedThrough = false;
+        }
+
+        if (hit.transform.tag != "Player")
+            if (moveAmount < 0f && hit.transform.position.y < 0f || moveAmount > 0f && hit.transform.position.y > 0f)
+                if (!hit.collider.isTrigger && hit.transform.tag != "Portal")
+                    passedThrough = true;
+    }
+    else if (passedThrough)
+        search = false;
+
+    transform.position -= new Vector3(0f, moveAmount);
+
+    yield return new WaitForSeconds(0.01f);
+
+    if (!search)
+    {
+        Instantiate(reviveSpot, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    else
+        Raycast();
+*/
