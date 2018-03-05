633963834049a4f670c73cc1da7b270efa7f7c18
Zufixx <filip.0717@hotmail.com>
2018-03-05 16:47:18 +0100

    Add GravityFlip script

---
+++ Assets/Scripts/GravityFlip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GravityFlip : MonoBehaviour {
+
+    Rigidbody2D rb;
+    [SerializeField]
+    private bool flipGravity = true;
+    [SerializeField]
+    private bool flipLocalScale = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale *= -1f;
+        transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y * -1f);
+	}
+}
