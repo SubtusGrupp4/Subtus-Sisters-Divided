a2f3b960e21b78dbdbb44582730099127a862a84
Zufixx <filip.0717@hotmail.com>
2018-03-07 15:26:30 +0100

    Fixed AudioZoneStandard

--- Assets/Scripts/FMOD/AudioZoneStandard.cs
+++ Assets/Scripts/FMOD/AudioZoneStandard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class AudioZonesStandard : MonoBehaviour
+public class AudioZoneStandard : MonoBehaviour
 {
     [System.Serializable]
     private struct Parameter
