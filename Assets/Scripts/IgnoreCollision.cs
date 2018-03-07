28bdd1eb95f5c2299d45e490661ea60ebda2e723
Xiao Liang <35493674+Diamondspader@users.noreply.github.com>
2018-03-07 13:57:09 +0100

    add script to make object ignore player

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour {
+
+	private GameObject[] ignoreObject;
+
+	// Use this for initialization
+	void Start () {
+		ignoreObject = GameObject.FindGameObjectsWithTag ("Player");
+		foreach (GameObject IO in ignoreObject)
+			Physics2D.IgnoreCollision (IO.GetComponent<Collider2D> (), this.GetComponent<Collider2D>(), true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
