fd120e445d0e59cbd12d0c434473b7f1722ce7db
Zufixx <filip.0717@hotmail.com>
2018-03-08 14:00:52 +0100

    Add arrow for aiming

---
+++ Assets/Scripts/ArrowPointTo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPointTo : MonoBehaviour
+{
+
+    private string rightXAxis = "Horizontal_Fire";
+    private string rightYAxis = "Vertical_Fire";
+
+    [SerializeField]
+    private Controller controller;
+
+    [SerializeField]
+    Vector2 direction;
+
+    public bool showArrow = false;
+
+    private SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start ()
+    {
+        string controllerCode;
+
+        controllerCode = controller == Controller.Player1 ? "_C1" : "_C2";
+
+        rightXAxis += controllerCode;
+        rightYAxis += controllerCode;
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showArrow)
+        {
+            float x = Input.GetAxisRaw(rightXAxis);
+            float y = Input.GetAxisRaw(rightYAxis);
+
+            if (y < 0f)
+                x = -x;
+
+            direction = new Vector2(x, y);
+            Quaternion rotation = Quaternion.LookRotation(direction.normalized, Vector2.right);
+            transform.rotation = new Quaternion(0f, 0f, rotation.x, rotation.w);
+        }
+    }
+
+    public void ShowArrow(bool show)
+    {
+        sr.enabled = show;
+        showArrow = show;
+    }
+}
---
+++ Assets/Scripts/PositionParent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionParent : MonoBehaviour {
+
+    [SerializeField]
+    private Transform parent;
+    [SerializeField]
+    private Vector3 offset;
+	
+	void Update ()
+    {
+        transform.position = parent.position + offset;
+	}
+}
