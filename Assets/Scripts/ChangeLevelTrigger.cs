61720b1aeee9dd0cfe33636a43ee905ba1df09df
Filip Bergkvist <filip.0717@hotmail.com>
2018-03-17 18:39:53 +0100

    New player prefabs, hopefully fixing the "Missing Prefab" problem. End of level trigger

---
+++ Assets/Scripts/ChangeLevelTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevelTrigger : MonoBehaviour
+{
+    bool topHasEntered = false;
+    bool botHasEntered = false;
+    [SerializeField]
+    private int levelId = 0;
+    [SerializeField]
+    private bool bothHaveToStay = true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (collision.transform == GameManager.instance.playerTop)
+                topHasEntered = true;
+            if (collision.transform == GameManager.instance.playerBot)
+                botHasEntered = true;
+
+            if (topHasEntered && botHasEntered)
+                GameManager.instance.ChangeLevel(levelId);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (bothHaveToStay)
+        {
+            if (collision.tag == "Player")
+            {
+                if (collision.transform == GameManager.instance.playerTop)
+                    topHasEntered = false;
+                if (collision.transform == GameManager.instance.playerBot)
+                    botHasEntered = false;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, GetComponent<BoxCollider2D>().size);
+    }
+}
---
+++ Assets/Scripts/SwitchPlayers.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchPlayers : MonoBehaviour
+{
+    public static SwitchPlayers instance;
+
+    [Header("References")]
+    [SerializeField]
+    private InstantiateGravityBomb antiGrav;
+    [SerializeField]
+    private InstantiateGravityBomb grav;
+
+    [Header("Animation")]
+    [SerializeField]
+    private Image switchImage;
+    [SerializeField]
+    private Sprite[] spriteSequence;
+    private int spriteIndex = 0;
+    private bool doAnimation = false;
+
+    [Header("Audio")]
+    [SerializeField]
+    private AudioClip staticClip;
+    private AudioSource source;
+
+    [Header("Dialogue")]
+    [SerializeField]
+    private float startTimer = 2f;
+    [SerializeField]
+    private float waitTime = 2f;
+    private Dialogue[] dialogues;
+
+    private void Start()
+    {
+        CreateSingleton();
+        source = GetComponent<AudioSource>();
+        source.clip = staticClip;
+        dialogues = GetComponents<Dialogue>();
+    }
+
+    public void StartSwitch()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    private IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(startTimer);
+        switchImage.gameObject.SetActive(true);
+        doAnimation = true;
+        source.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+            StartSwitch();
+    }
+
+    private void FixedUpdate()
+    {
+        if (doAnimation)
+        {
+            if (spriteIndex < spriteSequence.Length)
+            {
+                switchImage.sprite = spriteSequence[spriteIndex];
+                spriteIndex++;
+            }
+            else
+            {
+                switchImage.gameObject.SetActive(false);
+                DoSwitch();
+                doAnimation = false;
+                spriteIndex = 0;
+                StartCoroutine(DialogueTimer());
+            }
+        }
+    }
+
+    private void DoSwitch()
+    {
+        Transform top = GameManager.instance.playerTop;
+        Transform bot = GameManager.instance.playerBot;
+
+        Vector3 temp = top.position;
+        top.position = bot.position;
+        bot.position = temp;
+
+        Transform[] players = { top, bot };
+
+        foreach(Transform player in players)
+        {
+            player.GetComponent<PlayerController>().Flip();
+            player.GetComponent<PullBoxes>().Flip();
+            player.GetComponent<PlayerController>().PreventInput(false);
+        }
+    }
+
+    private void CreateSingleton()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private IEnumerator DialogueTimer()
+    {
+        yield return new WaitForSeconds(waitTime);
+        if(dialogues.Length != 0)
+            DialogueManager.instance.FetchDialogue(dialogues);  // Send the dialogue to the DialogueManager when triggered
+    }
+}
